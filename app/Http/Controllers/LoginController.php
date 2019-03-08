41f22aab12743d2316e8f4e78d303a6a3eb94782
Simon <6692223@qq.com>
2019-03-08 14:39:07 +0800

    video 6-6 end

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -15,12 +15,27 @@ class LoginController extends Controller
     // 登陆行为
     public function login()
     {
+        // 验证
+        $this->validate(request() , [
+           'email' => 'required|email',
+           'password' => 'required|min:5|max:16',
+           'is_remeber' => 'integer'
+        ]);
+        // 逻辑
+        $user = request(['email' , 'password']);
+        $is_remeber = boolval(request('is_remember'));
+        if(\Auth::attempt($user , $is_remeber)) {
+            return redirect('/posts');
+        }
+        // 渲染
+        return \Redirect::back()->withErrors('邮箱密码不匹配');
 
     }
 
     // 登出行为
     public function logout()
     {
-        
+        \Auth::logout();
+        return redirect('/login');
     }
 }
