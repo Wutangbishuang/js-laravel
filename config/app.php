1a1df06a028ebc671b845e15924952bbb00d9cfd
Simon <6692223@qq.com>
2019-03-09 23:32:19 +0800

    9-6 end

--- config/app.php
+++ config/app.php
@@ -176,7 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        //Scout
+        Laravel\Scout\ScoutServiceProvider::class,
+        ScoutEngines\Elasticsearch\ElasticsearchProvider::class,
     ],
 
     /*
