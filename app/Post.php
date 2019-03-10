1b6d27483e580564252bc27368fc3ea0c1a78a9b
Simon <6692223@qq.com>
2019-03-10 19:51:22 +0800

    video 9-8 end

--- app/Post.php
+++ app/Post.php
@@ -3,11 +3,28 @@
 namespace App;
 
 use App\BaseModel;
+use Laravel\Scout\Searchable;
 
 class Post extends BaseModel
 {
 //    protected $guarded; // 不可以注入的字段
 //    protected $fillable  // 可以注入的字段
+
+    use Searchable;
+    // 定义索引里面的type
+    public function searchableAs()
+    {
+        return "post";
+    }
+
+    // 定义有哪些字段需要搜索
+    public function toSearchableArray()
+    {
+        return [
+            'title' => $this->title,
+            'content' => $this->content,
+        ];
+    }
 
     // 关联用户
     public function user()
