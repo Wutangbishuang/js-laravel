659a9e9a13b2791c65c0a553258f8d2159d5e99f
Simon <6692223@qq.com>
2019-03-13 18:50:10 +0800

    video chart 11 end

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\BaseModel;
+use Illuminate\Database\Eloquent\Builder;
 use Laravel\Scout\Searchable;
 
 class Post extends BaseModel
@@ -48,5 +49,24 @@ class Post extends BaseModel
     public function zans()
     {
         return $this->hasMany(\App\Zan::class);
+    }
+
+    // 属于某个作者的文章
+    public function scopeAuthorBy(\Illuminate\Database\Eloquent\Builder $query,$user_id)
+    {
+        return $query->where('user_id' , $user_id);
+    }
+
+    public function postTopics()
+    {
+        return $this->hasMany(\App\PostTopic::class , 'post_id' , 'id');
+    }
+
+    // 不属于某个作者的文章
+    public function scopeTopicNotBy(Builder $query , $topic_id)
+    {
+        return $query->doesntHave('postTopics' , 'and' , function($q) use($topic_id) {
+            $q->where('topic_id' , $topic_id);
+        });
     }
 }
--- app/Topic.php
+++ app/Topic.php
@@ -6,5 +6,15 @@ use App\BaseModel;
 
 class Topic extends BaseModel
 {
-    //
+    // 属于这个标题的所有文章
+    public function posts()
+    {
+        return $this->belongsToMany(\App\Post::class,'post_topics' ,'topic_id' , 'post_id');
+    }
+
+    // 专题的文章数,用于withCount
+    public function postTopics()
+    {
+        return $this->hasMany(\App\PostTopic::class ,'topic_id');
+    }
 }
