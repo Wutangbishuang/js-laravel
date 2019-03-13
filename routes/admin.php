bbd62451a7b1ce74990936c0e409c31727ffb335
Simon <6692223@qq.com>
2019-03-13 23:24:08 +0800

    12-8end

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,11 @@
 <?php
 Route::group(['prefix' => 'admin'] , function(){
-    Route::get('/login' , function(){
-       return "this is login";
-    });
+    // 登陆页面
+    Route::get('/login' , '\App\Admin\Controllers\LoginController@index');
+    // 登陆行为
+    Route::post('/login' , '\App\Admin\Controllers\LoginController@login');
+    // 登出行为
+    Route::get('/logout' , '\App\Admin\Controllers\LoginController@logout');
+    // 首页
+    Route::get('/home' , '\App\Admin\Controllers\HomeController@index');
 });
