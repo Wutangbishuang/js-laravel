b64b38cd496d2106985b628e0741ea46ab2bd87e
Simon <6692223@qq.com>
2019-03-14 16:27:00 +0800

    14-3 end

--- app/Post.php
+++ app/Post.php
@@ -69,4 +69,15 @@ class Post extends BaseModel
             $q->where('topic_id' , $topic_id);
         });
     }
+
+    // 全局scope的方式
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope("avaiable" , function(Builder $builder)
+        {
+            $builder->whereIn('status' , [0,1]);
+        });
+    }
 }
