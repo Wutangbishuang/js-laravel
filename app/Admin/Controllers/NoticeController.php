e2e21e17bf5bdda6407a8f699304cea4a77c88b5
Simon <6692223@qq.com>
2019-03-16 00:58:42 +0800

    17 end

---
+++ app/Admin/Controllers/NoticeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Jobs\SendMessage;
+use App\Notice;
+
+class NoticeController extends Controller
+{
+    public function index()
+    {
+        $notices = Notice::all();
+        return view('admin/notice/index' , compact('notices'));
+    }
+
+    public function create()
+    {
+        return view('admin/notice/create');
+    }
+
+    public function store()
+    {
+        $this->validate(request() , [
+            'title' => 'required|string',
+            'content' => 'required|string'
+        ]);
+        $notice = Notice::create(request(['title' , 'content']));
+
+        $this->dispatch(new \App\Jobs\SendMessage($notice));
+        return redirect("/admin/notices");
+    }
+
+}
---
+++ app/Notice.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use App\BaseModel;
+
+class Notice extends BaseModel
+{
+    //
+}
